557cd1807716d7fa3101f4d8af66cdaf7e45e25c
Alan O'Connell <35573689+paoconnell@users.noreply.github.com>
2019-03-31 14:45:33 +0200

    Create test configs with name

--- example/module_a/run.py
+++ example/module_a/run.py
@@ -5,4 +5,8 @@ import simtools
 root = os.path.dirname(__file__)
 sim = simtools.Simulation()
 sim.add_source_files(os.path.join(root, "tst", "*.sv"))
+
+sim.add_config("module_a_tb", "name1")
+sim.add_config("module_a_tb", "name2")
+
 sim.run()
--- python/simtools/simulation.py
+++ python/simtools/simulation.py
@@ -11,5 +11,14 @@ class Simulation(object):
     def add_source_files(self, source_files):
         self.lib.add_source_files(source_files)
 
+    def add_config(self, testbench, name):
+        tbs = self.lib.get_test_benches("*"+testbench)
+        if len(tbs) > 1:
+            raise ValueError("Testbench {:s} found multiple times!".format(testbench))
+        if tbs[0].name != testbench:
+            raise ValueError("Testbench {:s} not found!".format(testbench))
+        tb = tbs[0]
+        tb.add_config(name)
+
     def run(self, coverage=False):
         self.ui.main()
